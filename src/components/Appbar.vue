27ee6569eb5e8677dfa43603dbd5836c5b9f19ba
wrj2009 <wrj-2009@qq.com>
2022-04-10 21:14:13 +0800

    修改图标、标题和页面内容等

---
+++ src/components/Appbar.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app-bar app>
+    <div>
+      <!-- <img src="../assets/GitScratch-icon-black.png" v-if="!dark_theme" width="40" /> -->
+    </div>
+
+    <v-spacer></v-spacer>
+
+    <v-btn icon v-on:click="toggle_theme">
+      <v-icon>mdi-brightness-6</v-icon>
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+  export default {
+    name: 'Appbar',
+
+    data: () => ({
+      dark_theme: false,
+    }),
+
+    methods: {
+      toggle_theme: function() {
+        this.$vuetify.theme.dark = !this.$vuetify.theme.dark;
+        this.dark_theme = !this.dark_theme;
+      },
+    },
+  }
+</script>
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  chainWebpack: config => {
+    config
+      .plugin("html")
+      .tap(args => {
+        args[0].title = "GitScratch";
+        return args;
+      }
+    )
+  }
 }
