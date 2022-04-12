e1c72470d879456dfabe145da05805e62e36165b
wrj2009 <wrj-2009@qq.com>
2022-04-12 21:15:09 +0800

    修改 Appbar

--- src/components/Appbar.vue
+++ src/components/Appbar.vue
@@ -4,10 +4,13 @@
       <img v-if="!dark_theme" src="../assets/GitScratch-icon-black.svg" width="40" />
       <img v-if="dark_theme" src="../assets/GitScratch-icon-white.svg" width="40" />
     </div>
-    <h2>GitScratch</h2>
+    <h2 style="margin-right: 8px;">GitScratch</h2>
+    <v-btn text color="blue" href="/">首页</v-btn>
 
     <v-spacer></v-spacer>
 
+    <v-btn color="blue" dark style="margin-right: 8px;">注册</v-btn>
+    <v-btn color="transparent" style="margin-right: 8px;">登录</v-btn>
     <v-btn icon v-on:click="toggle_theme">
       <v-icon>mdi-brightness-6</v-icon>
     </v-btn>
