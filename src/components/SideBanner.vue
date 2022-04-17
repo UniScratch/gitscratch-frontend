31a333428b7fd7eef9e5947a6a1bf268d9dfbc86
MarkChai <87580250+HydroGest@users.noreply.github.com>
2022-04-17 22:55:34 +0800

    增加内容垂直居中

--- src/components/SideBanner.vue
+++ src/components/SideBanner.vue
@@ -3,7 +3,9 @@
         <v-card-title class="text-h5" style="display: block;">欢迎</v-card-title>
 
         <v-card-text>
+            <v-container style="margin:auto;left:0;right:0;top:0;bottom:0;">
             美好的一天，从咕咕咕开始
+            </v-container>
         </v-card-text>
 
         <v-card-actions style="position: absolute; bottom: 0; left: 0; right: 0;">
