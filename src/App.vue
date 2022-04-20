4ae9b3a2e678dc29c79e206c645e7b51e210b609
wrj2009 <wrj-2009@qq.com>
2022-04-20 21:20:18 +0800

    添加 ProjectCard

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
         <v-main>
             <Appbar />
             <v-container>
-                <v-row>
+                <v-row style="margin-bottom: 8px;">
                     <v-col cols="9" height="350">
                         <Carousel style="height: 100% !important;"/>
                         <!-- <v-sheet rounded="lg" min-height="268"></v-sheet> -->
@@ -14,6 +14,10 @@
                         <!-- <v-sheet min-height="70vh" rounded="lg"></v-sheet> -->
                     </v-col>
                 </v-row>
+                <div style="margin-bottom: 24px;">
+                    <p class="text-h5">精选作品</p>
+                    <ProjectCard /><ProjectCard /><ProjectCard /><ProjectCard /><ProjectCard />
+                </div>
             </v-container>
         </v-main>
     </v-app>
@@ -23,6 +27,7 @@
 import Appbar from './components/Appbar.vue'
 import Carousel from './components/Carousel.vue'
 import SideBanner from './components/SideBanner.vue'
+import ProjectCard from './components/ProjectCard.vue'
 
 export default {
     name: 'App',
@@ -30,7 +35,8 @@ export default {
     components: {
         Appbar,
         Carousel,
-        SideBanner
+        SideBanner,
+        ProjectCard,
     },
 
     data: () => ({
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,36 @@
+<template>
+    <v-card style="margin-right: 8px; width: calc(20% - 8px); display: inline-block;">
+        <v-card-title style="padding-bottom: 0;">{{ project_title }}</v-card-title>
+        <v-card-actions>
+            <v-btn text v-bind:href="'/user/' + project_author">
+                <v-avatar size="32">
+                    <img v-bind:src="project_author_avatar" />
+                </v-avatar>
+                <span style="margin-left: 8px;">{{ project_author }}</span>
+            </v-btn>
+
+        </v-card-actions>
+        <v-img lazy-src="/GitScratch-icon-white.svg" v-bind:src="project_thumbnail" style="width: 100%;"></v-img>
+    </v-card>
+</template>
+<script>
+export default {
+    name: 'ProjectCard',
+
+    data: () => ({
+        project_title: "标题",
+        project_author: "作者",
+        project_author_avatar: "/favicon.ico",
+        project_thumbnail: "https://sdfsdf.dev/480x360.jpg,FFFFFF,000000",
+
+    }),
+
+    methods: {
+        toggle_theme: function () {
+            this.$vuetify.theme.dark = !this.$vuetify.theme.dark;
+            this.dark_theme = !this.dark_theme;
+        },
+    },
+}
+
+</script>
