2f72eec27eb89018c938aa5a90194fcb073cd449
TimFang <mc13735292967@163.com>
2022-04-23 09:58:22 +0800

    启用 terser 和 css 压缩

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
 const TerserPlugin = require("terser-webpack-plugin");
+// const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = {
+  productionSourceMap: false,
   transpileDependencies: [
     'vuetify'
   ],
@@ -12,17 +15,31 @@ module.exports = {
         return args;
       })
   },
-  publicPath: './'
-};
-
-configureWebpack: {
+  publicPath: './',
+  configureWebpack: {
     optimization: {
+      minimize: true,
       minimizer: [
         new TerserPlugin({
           parallel: true,
-          terserOptions: {
+          terserOptions: { // https://github.com/terser/terser
+            compress: {
+              drop_console: true,
+              arguments: true,
+              passes: 2
+            },
+            format: {
+              comments: false,
+              max_line_len: 1024
+            }
           }
-        })
+        }),
+        new CssMinimizerPlugin({
+          parallel: true,
+        }),
       ]
-    }
-}
+    },
+    // plugins: [new MiniCssExtractPlugin()],
+
+  }
+};
