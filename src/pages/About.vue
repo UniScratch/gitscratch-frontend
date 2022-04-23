e1fdd16b3a7e73a4f457f442b12ab2d10700da6e
TimFang <mc13735292967@163.com>
2022-04-23 11:27:08 +0800

    H2O
    Signed-off-by: TimFang <mc13735292967@163.com>

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -24,7 +24,20 @@ export default {
     }),
     methods: {
         renderMd: function () {
-            var md = "# About GitScratch"
+            var md = `
+# About GitScratch  
+GitScratch（极社）是一个 Git 风格的 Scratch 社区，正在开发中。  
+# Contributor list  
+项目提出: markchai, dispure  
+主要开发者: wrj2009, TimFang4162  
+金主:  
+其他贡献者: frank-782  
+还有，你。  
+  
+~~一个都跑不了~~
+
+
+            `
             return marked.parse(md);
         }
     },
