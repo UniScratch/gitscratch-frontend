31ed3021540865abc320e2276a7c26560bdca669
TimFang <mc13735292967@163.com>
2022-04-23 14:43:36 +0800

    修复 vue-router 错误

--- src/main.js
+++ src/main.js
@@ -1,30 +1,13 @@
 import Vue from 'vue'
-import App from './pages/App.vue'
-import About from './pages/About.vue'
+import App from './App.vue'
 import vuetify from './plugins/vuetify'
+import router from "./router/index.js";
 
 Vue.config.productionTip = false
 
-const NotFound = {
-  template: '<p>Page not found</p>'
-}
-const routes = {
-  '/': App,
-  '/about': About
-}
-
 new Vue({
   el: '#app',
-  data: {
-    currentRoute: window.location.pathname
-  },
-  computed: {
-    ViewComponent() {
-      return routes[this.currentRoute] || NotFound
-    }
-  },
   vuetify,
-  render(h) {
-    return h(this.ViewComponent)
-  }
+  router,
+  render: h => h(App)
 }).$mount('#app')
