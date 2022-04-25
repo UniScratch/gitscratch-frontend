7a48fdb3c4a9f460292148bf5f83cb7440a501e8
Dispure <61584736+Dispure@users.noreply.github.com>
2022-04-26 00:43:30 +0800

    Rollback bad merge.

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -15,10 +15,9 @@ export default {
 # 关于 GitScratch  
 GitScratch（极社）是一个 Git 风格的 Scratch 社区，正在开发中。  
 
-项目提出：[@HydroGest](https://github.com/HydroGest) @Dispure 
-
+项目提出：[@HydroGest](https://github.com/HydroGest) @Dispure  
 金主：❤️  
-主要贡献者：[@Tim-Fang](https://github.com/Tim-Fang) [@wrj2009](https://github.com/wrj2009) [@HydroGest](https://github.com/HydroGest)  
+主要贡献者：[@Tim-Fang](https://github.com/Tim-Fang) [@wrj2009](https://github.com/wrj2009) [@HydroGest](https://github.com/HydroGest) @Dispure  
 其他贡献者：[@frank-782](https://github.com/frank-782)  
 还有，你。  
 ~~一个都跑不了~~
