78d539d66e21e19a6ab26f7084d7834ee01a84a0
Dispure <61584736+Dispure@users.noreply.github.com>
2022-04-26 08:42:21 +0800

    Format Home.vue
    Format the code of Home.vue .

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,18 +1,18 @@
 <template>
     <div>
-    <v-row style="margin-bottom: 8px;">
-        <v-col cols="12" sm="9">
-            <Carousel />
-            <!-- <v-sheet rounded="lg" min-height="268"></v-sheet> -->
-        </v-col>
+        <v-row style="margin-bottom: 8px;">
+            <v-col cols="12" sm="9">
+                <Carousel />
+                <!-- <v-sheet rounded="lg" min-height="268"></v-sheet> -->
+            </v-col>
 
-        <v-col cols="12" sm="3">
-            <SideBanner />
-            <!-- <v-sheet min-height="70vh" rounded="lg"></v-sheet> -->
-        </v-col>
-    </v-row>
-    <ProjectGroup title="精选作品" />
-    <ProjectGroup title="热门作品" />
+            <v-col cols="12" sm="3">
+                <SideBanner />
+                <!-- <v-sheet min-height="70vh" rounded="lg"></v-sheet> -->
+            </v-col>
+        </v-row>
+        <ProjectGroup title="精选作品" />
+        <ProjectGroup title="热门作品" />
     </div>
 </template>
 
