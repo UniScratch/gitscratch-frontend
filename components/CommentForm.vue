29d2cb664b2ba526beb0dcfdf12bdcfea5a5fb9d
Dispure <61584736+Dispure@users.noreply.github.com>
2022-06-28 19:27:50 +0800

    Bug Fixes

--- components/CommentForm.vue
+++ components/CommentForm.vue
@@ -1,37 +1,39 @@
 <template>
-  <v-form ref="form" v-model="valid" style="margin-bottom: 16px;" lazy-validation>
+  <span>
     <v-switch
       v-if="isAuthor"
       label="允许评论"
       @click="changeCommentAllow()"
     />
-    <v-textarea
-      v-model="comment_content"
-      :label="allowComment ? '评论' : '评论区已关闭'"
-      counter="1024"
-      auto-grow
-      rows="2"
-      outlined
-      style="border-radius: 4px;"
-      :rules="[commentContentRules.comment]"
-      :disabled="!allowComment"
-    />
-    <v-btn
-      :disabled="!valid||!allowComment"
-      color="primary"
-      depressed
-      rounded
-      style="transition: background-color 0.5s;"
-      @click="comment"
-    >
-      发布
-    </v-btn>
-    <transition name="slide-y-reverse-transition">
-      <v-btn v-if="comment_content || !valid" text color="accent" rounded @click="reset">
-        重置
+    <v-form ref="form" v-model="valid" style="margin-bottom: 16px;" lazy-validation>
+      <v-textarea
+        v-model="comment_content"
+        :label="allowComment ? '评论' : '评论区已关闭'"
+        counter="1024"
+        auto-grow
+        rows="2"
+        outlined
+        style="border-radius: 4px;"
+        :rules="[commentContentRules.comment]"
+        :disabled="!allowComment"
+      />
+      <v-btn
+        :disabled="!valid||!allowComment"
+        color="primary"
+        depressed
+        rounded
+        style="transition: background-color 0.5s;"
+        @click="comment"
+      >
+        发布
       </v-btn>
-    </transition>
-  </v-form>
+      <transition name="slide-y-reverse-transition">
+        <v-btn v-if="comment_content || !valid" text color="accent" rounded @click="reset">
+          重置
+        </v-btn>
+      </transition>
+    </v-form>
+  </span>
 </template>
 <script>
 export default {
