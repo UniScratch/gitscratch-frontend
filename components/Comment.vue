c5edb9bd08008369cf93dadf4aa8cb6895eec6f8
Dispure <61584736+Dispure@users.noreply.github.com>
2022-06-28 12:08:29 +0800

    Get ready for adding v-for to the Comment area

--- components/Comment.vue
+++ components/Comment.vue
@@ -1,22 +1,22 @@
 <template>
   <v-card style="padding: 16px;border-radius: 10px !important;" class="d-flex">
     <v-avatar>
-      <v-img :src="comment_author_avatar" />
+      <v-img :src="comment_list[1]" />
     </v-avatar>
     <v-divider vertical style="margin: 0 8px;" />
     <div class="flex-grow-1 overflow-auto" style="border-radius: 0px;">
-      <router-link :to="'/users/' + comment_author_name">
-        {{ comment_author_name }}
+      <router-link :to="'/users/' + comment_list[2]">
+        {{ comment_list[2] }}
       </router-link>
-      <v-tooltip v-if="comment_author_is_verified" bottom>
+      <v-tooltip v-if="comment_list[4]" bottom>
         <template #activator="{ on, attrs }">
-          <v-icon style="margin-top: -2px;" :color="comment_author_verify_color" v-bind="attrs" size="20" v-on="on">
+          <v-icon style="margin-top: -2px;" :color="comment_list[6]" v-bind="attrs" size="20" v-on="on">
             mdi-check-decagram-outline
           </v-icon>
         </template>
-        <span>{{ comment_author_verify_categories }}</span>
+        <span>{{ comment_list[5] }}</span>
       </v-tooltip>
-      <span style="margin-left: 8px;" class="grey-text">{{ comment_time }}</span>
+      <span style="margin-left: 8px;" class="grey-text">{{ comment_list[3] }}</span>
       <v-divider style="margin: 8px 0;" />
       <MarkdownRender />
     </div>
@@ -31,12 +31,7 @@ export default {
     MarkdownRender
   },
   data: () => ({
-    comment_author_avatar: '/GitScratch-icon-background-blue.svg',
-    comment_author_name: '作者',
-    comment_time: dateStr,
-    comment_author_is_verified: true,
-    comment_author_verify_categories: '社区官方认证',
-    comment_author_verify_color: 'rgba(33,150,243)'
+    comment_list: ['1', '/GitScratch-icon-background-blue.svg', '作者', dateStr, true, '社区官方认证', 'rgba(33,150,243)']
   }),
   methods: {
   }
