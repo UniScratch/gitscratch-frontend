750855aaf124795be16070f96387d884364a9f82
Dispure <61584736+Dispure@users.noreply.github.com>
2022-06-28 19:01:39 +0800

    Some changes
    我是傻逼

--- components/CommentForm.vue
+++ components/CommentForm.vue
@@ -7,24 +7,24 @@
     />
     <v-textarea
       v-model="comment_content"
-      :label="allowComment ? '评论' : '评论区已关闭'"
+      :label="allowComment ? '评论区已关闭' : '评论'"
       counter="1024"
       auto-grow
       rows="2"
       outlined
       style="border-radius: 4px;"
       :rules="[commentContentRules.comment]"
-      :disabled="!allowComment"
+      :disabled="allowComment"
     />
     <v-btn
-      :disabled="!valid||!allowComment"
+      :disabled="!valid||allowComment"
       color="primary"
       depressed
       rounded
       style="transition: background-color 0.5s;"
       @click="comment"
     >
-      发布{{ allow_comment }}
+      发布
     </v-btn>
     <transition name="slide-y-reverse-transition">
       <v-btn v-if="comment_content || !valid" text color="accent" rounded @click="reset">
@@ -43,7 +43,7 @@ export default {
       comment: v => !!v || '留言内容不能为空'
     },
     comment_content: '',
-    allowComment: false,
+    allowComment: true,
     isAuthor: true
   }),
   methods: {
