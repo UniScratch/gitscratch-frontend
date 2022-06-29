921d6dec4c24edeac5bea2734adf635096c65d46
Dispure <61584736+Dispure@users.noreply.github.com>
2022-06-29 14:34:24 +0800

    Fix issue #46

--- components/ProjectCard.vue
+++ components/ProjectCard.vue
@@ -1,27 +1,29 @@
 <template>
-  <v-card v-ripple :to="'/projects/' + project_id">
+  <v-card v-ripple :to="'/projects/' + project_id" @mouseenter="upHeight()" @mouseleave="downHeight()">
     <v-hover v-slot="{ hover }">
       <v-img lazy-src="/GitScratch-project-thumbnail-grey.svg" :src="project_thumbnail" style="width: 100%;">
+        <span ref="viewTimes" class="rounded-pill" style="left: 3%; position: absolute; background-color: #000000; opacity: .4; bottom: 5%; color: white; font-size: 10px; padding: 0 5px; transition: bottom 0.4s;">
+          <v-icon style="font-size: 10px;color: white;margin-bottom: 1px;">mdi-eye-outline</v-icon>
+          345
+        </span>
         <v-fade-transition>
-          <v-card
-            v-if="hover"
-            style="height: 100%; border-radius: 0px;"
-            class="project-thumbnail-info"
-          >
-            <v-container style="font-size: 1vw;">
-              <v-icon>mdi-eye-outline</v-icon>
-              345
-              <br>
-              <v-icon>mdi-star-outline</v-icon>
-              456
-              <br>
-              <v-icon>mdi-source-branch</v-icon>
-              567
-              <br>
-              <v-icon>mdi-calendar-outline</v-icon>
+          <v-container v-if="hover" style="font-size: 1vw;">
+            <span class="rounded-pill" style="left: 3%; position: absolute; background-color: #000000; opacity: .4; bottom: 5%; color: white; font-size: 10px; padding: 0 5px;">
+              <v-icon style="font-size: 10px;color: white;margin-bottom: 1px;">mdi-history</v-icon>
               2022/5/28 14:39
-            </v-container>
-          </v-card>
+            </span>
+            <span class="rounded-pill" style="left: 3%; position: absolute; background-color: #000000; opacity: .4; bottom: 25%; color: white; font-size: 10px; padding: 0 5px;">
+              <v-icon style="font-size: 10px;color: white;margin-bottom: 1px;">mdi-star-outline</v-icon>
+              456
+            </span>
+            <span class="rounded-pill" style="left: 3%; position: absolute; background-color: #000000; opacity: .4; bottom: 45%; color: white; font-size: 10px; padding: 0 5px;">
+              <v-icon style="font-size: 10px;color: white;margin-bottom: 1px;">mdi-source-branch</v-icon>
+              567
+            </span>
+
+            <!--<v-icon>mdi-calendar-outline</v-icon>
+            2022/5/28 14:39-->
+          </v-container>
           <!-- <div
             v-if="hover"
             color="primary"
@@ -61,6 +63,14 @@ export default {
     project_author_avatar: '/GitScratch-icon-background-blue.svg',
     project_thumbnail: 'https://sdfsdf.dev/480x360.jpg,E3F2FD,2196F3',
     project_id: 1
-  })
+  }),
+  methods: {
+    upHeight () {
+      this.$refs.viewTimes.style.bottom = '65%'
+    },
+    downHeight () {
+      this.$refs.viewTimes.style.bottom = '5%'
+    }
+  }
 }
 </script>
