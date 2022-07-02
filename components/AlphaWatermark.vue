67587b2e2c5abc2951e68bf9384423c62f37c782
Dispure <61584736+Dispure@users.noreply.github.com>
2022-07-02 22:47:19 +0800

    Update AlphaWatermark.vue

--- components/AlphaWatermark.vue
+++ components/AlphaWatermark.vue
@@ -1,14 +1,16 @@
 <template>
-  <img src="/alpha.svg" class="fixed-watermark">
+  <v-container>
+    <v-img src="/alpha.svg" class="fixed-watermark" />
+  </v-container>
 </template>
 <style>
 .fixed-watermark{
-    width: 20%;
+    width: 230px;
     height: auto;
-    position: fixed;
+    position: fixed !important;
     bottom: 5px;
     left: 5px;
-    opacity: .9;
+    opacity: 1;
     z-index: 1;
     pointer-events: none;
 }
