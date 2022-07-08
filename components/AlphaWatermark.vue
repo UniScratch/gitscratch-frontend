457f46beb672918bca38333bfdcb12d89db80cc3
TimFang <mc13735292967@163.com>
2022-07-08 12:53:29 +0800

    修改水印样式
    Signed-off-by: TimFang <mc13735292967@163.com>

--- components/AlphaWatermark.vue
+++ components/AlphaWatermark.vue
@@ -1,16 +1,20 @@
 <template>
   <v-container>
-    <v-img src="/alpha.svg" class="fixed-watermark" />
+    <span class="fixed-watermark">
+      <!-- <v-icon size="12">mdi-star-four-points-outline</v-icon> -->
+      开发中版本, 不代表最终使用体验与品质
+    </span>
   </v-container>
 </template>
 <style>
 .fixed-watermark{
-    width: 230px;
+    /* width: 230px; */
+    font-size: 10px;
     height: auto;
     position: fixed !important;
     bottom: 5px;
     left: 5px;
-    opacity: 1;
+    opacity: 0.5;
     z-index: 1;
     pointer-events: none;
 }
