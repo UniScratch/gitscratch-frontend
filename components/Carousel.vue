aff3f65fd036dc078e47bc6197c20e2799950299
TimFang <mc13735292967@163.com>
2022-07-08 14:04:53 +0800

    修复banner图片加载
    Signed-off-by: TimFang <mc13735292967@163.com>

--- components/Carousel.vue
+++ components/Carousel.vue
@@ -2,7 +2,7 @@
   <v-card>
     <v-window v-model="onboarding">
       <v-window-item v-for="n in length" :key="`card-${n}`">
-        <v-img src="/GitScratch-thumbnail.png" contain />
+        <v-img src="/GitScratch-thumbnail.png" eager contain />
         <div class="banner-text">
           <v-list-item-title class="banner-text-title">
             GitScratch
