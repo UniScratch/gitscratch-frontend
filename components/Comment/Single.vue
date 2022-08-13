8d4cb20cafab3cd032c30655276fa0af3d266ab7
TimFang <mc13735292967@163.com>
2022-08-13 21:02:34 +0800

    添加评论操作
    Signed-off-by: TimFang <mc13735292967@163.com>

--- components/Comment/Single.vue
+++ components/Comment/Single.vue
@@ -4,7 +4,7 @@
       <v-img :src="commentData.user.avatar" />
     </v-avatar>
     <v-divider vertical style="margin: 0 8px;" />
-    <div class="flex-grow-1 overflow-auto" style="border-radius: 0px;">
+    <div v-if="commentData.status === 0" class="flex-grow-1 overflow-auto" style="border-radius: 0px;">
       <router-link :to="'/users/' + commentData.user.id">
         {{ commentData.user.name }}
       </router-link>
@@ -26,8 +26,86 @@
       </v-tooltip> -->
       <span style="margin-left: 8px;" class="grey-text">{{ new Date(commentData.time * 1000).format("yyyy-MM-dd hh:mm:ss") }}</span>
       <span style="margin-left: 8px;" class="grey-text">{{ commentData.region }}</span>
+      <!-- <v-spacer /> -->
+      <v-menu bottom offset-y transition="slide-y-transition">
+        <template #activator="{ on, attrs }">
+          <v-btn
+            icon
+            plain
+            small
+            class="d-flex float-right mr-1"
+            v-bind="attrs"
+            v-on="on"
+          >
+            <v-icon>mdi-dots-horizontal</v-icon>
+          </v-btn>
+        </template>
+        <v-card class="cardblur">
+          <v-list dense color="transparent">
+            <v-list-item link>
+              <v-list-item-icon>
+                <v-icon>mdi-reply-outline</v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title>回复</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+            <v-list-item link>
+              <v-list-item-icon>
+                <v-icon>mdi-link-variant</v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title>复制链接</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+            <v-list-item link>
+              <v-list-item-icon>
+                <v-icon>mdi-pencil-outline</v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title>编辑</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+            <v-list-item link>
+              <v-list-item-icon>
+                <v-icon>mdi-eye-off-outline</v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title>隐藏</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+            <v-list-item link>
+              <v-list-item-icon>
+                <v-icon>mdi-delete-outline</v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title>删除</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+            <v-list-item link>
+              <v-list-item-icon>
+                <v-icon>mdi-alert-octagon-outline</v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title>举报</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+        </v-card>
+      </v-menu>
+
       <v-divider style="margin: 8px 0;" />
       <MarkdownRender :content="commentData.comment" />
+    </div>
+    <div v-if="commentData.status === 1" class="flex-grow-1 overflow-auto" style="border-radius: 0px;">
+      <v-expansion-panels accordion>
+        <v-expansion-panel>
+          <v-expansion-panel-header>Item</v-expansion-panel-header>
+          <v-expansion-panel-content>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
     </div>
   </v-card>
 </template>
