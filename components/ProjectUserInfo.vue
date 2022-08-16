29d2be44b981d4597b9f03f1c4b529409b8ceb0a
TimFang <mc13735292967@163.com>
2022-08-16 20:32:05 +0800

    作品页
    Signed-off-by: TimFang <mc13735292967@163.com>

--- components/ProjectUserInfo.vue
+++ components/ProjectUserInfo.vue
@@ -1,35 +1,45 @@
 <template>
-  <v-card :to="'/users/' + projectAuthorId">
-    <v-list-item three-line>
+  <v-card>
+    <v-list-item two-line>
       <v-list-item-avatar
-        size="80"
+        size="60"
       >
         <v-avatar style="width: auto; height: auto; max-width: 200px;">
-          <v-img :src="projectAuthorAvatar" size="80" />
+          <v-img :src="userData.avatar" size="60" />
         </v-avatar>
       </v-list-item-avatar>
       <v-list-item-content>
         <v-list-item-title class="text-h5">
-          {{ projectAuthor }}
-          <v-tooltip v-if="projectAuthorIsVerified" bottom>
+          <nuxt-link :to="'/users/' + userData.id" class="project-title">
+            {{ userData.name }}
+          </nuxt-link>
+          <v-tooltip v-if="userData.verified !== 0" bottom>
             <template #activator="{ on, attrs }">
-              <v-icon style="margin-top: -5px;" :color="projectAuthorVerifyColor" v-bind="attrs" v-on="on">
+              <v-icon color="rgba(33,150,243)" v-bind="attrs" v-on="on">
                 mdi-check-decagram-outline
               </v-icon>
             </template>
-            <span>{{ projectAuthorVerifyCategories }}</span>
+            <span>社区官方认证</span>
           </v-tooltip>
-          <v-tooltip v-if="projectAuthorIsMuted" bottom>
+          <v-tooltip v-if="userData.muted !== 0" bottom>
             <template #activator="{ on, attrs }">
-              <v-icon style="margin-top: -5px;" color="rgba(255, 87, 34)" v-bind="attrs" v-on="on">
+              <v-icon color="rgb(255, 87, 34)" v-bind="attrs" v-on="on">
                 mdi-comment-remove-outline
               </v-icon>
             </template>
-            <span>作者被禁言, 剩余 {{ projectAuthorMuteRemain }} 天解禁</span>
+            <span>账户被禁言，{{ userData.muted }} 天后解禁</span>
+          </v-tooltip>
+          <v-tooltip v-if="userData.banned !== 0" bottom>
+            <template #activator="{ on, attrs }">
+              <v-icon color="rgb(238, 54, 37)" v-bind="attrs" v-on="on">
+                mdi-gavel
+              </v-icon>
+            </template>
+            <span>帐户被封禁，{{ userData.banned }} 天后解禁</span>
           </v-tooltip>
         </v-list-item-title>
-        <span class="level-span">Lv. {{ projectAuthorLevel }}</span>
-        <v-list-item-subtitle>Lorem sit deserunt adipisicing anim cupidatat mollit exercitation non mollit voluptate. Ad esse non veniam Lorem sint et. </v-list-item-subtitle>
+        <span class="level-span">Lv. {{ userData.level }}</span>
+        <v-list-item-subtitle>{{ userData.bio }}</v-list-item-subtitle>
       </v-list-item-content>
     </v-list-item>
 
@@ -44,7 +54,12 @@
 
 <script>
 export default {
-
+  props: {
+    userData: {
+      type: Object,
+      default: () => ({})
+    }
+  },
   data: () => ({
     projectAuthor: '作者',
     projectAuthorId: 1,
--- pages/projects/_id/index.vue
+++ pages/projects/_id/index.vue
@@ -3,14 +3,19 @@
     <v-col cols="12" xl="7" md="8">
       <v-card>
         <v-card-title class="text-h5">
-          {{ projectTitle }}
+          {{ data.title }}
+          <v-spacer />
+          <v-btn text>
+            <v-icon>mdi-pencil-outline</v-icon>
+            作品设置
+          </v-btn>
         </v-card-title>
         <v-card-text>
           <div style="display:flex;margin-bottom:10px;">
             <v-icon>mdi-eye-outline</v-icon>
-            &nbsp;{{ projectViews }}&nbsp;&nbsp;
+            &nbsp;{{ data.view }}&nbsp;&nbsp;
             <v-icon>mdi-calendar-outline</v-icon>
-            &nbsp;{{ projectUpdate }}&nbsp;&nbsp;
+            &nbsp;{{ new Date(data.updated_at * 1000).format("yyyy-MM-dd hh:mm:ss") }}&nbsp;&nbsp;
             <v-icon>mdi-update</v-icon>&nbsp;
             <nuxt-link to="/projects/1/commit/170ed1c/" class="text-color">
               {{ projectCommit }}
@@ -51,11 +56,113 @@
           </v-responsive>
           <LazyProjectPlayer v-if="loadPlayer" />
           <br>
-          <ProjectData />
+          <v-row>
+            <v-tooltip bottom>
+              <template #activator="{ on, attrs }">
+                <v-btn text v-bind="attrs" rounded v-on="on">
+                  <v-icon>
+                    mdi-star-outline
+                  </v-icon>
+                  {{ data.star }}
+                </v-btn>
+              </template>
+              <span>星标</span>
+            </v-tooltip>
+            <v-tooltip v-if="data.source" bottom>
+              <template #activator="{ on, attrs }">
+                <v-btn text v-bind="attrs" rounded v-on="on">
+                  <v-icon>
+                    mdi-source-branch
+                  </v-icon>
+                  234
+                </v-btn>
+              </template>
+              <span>改编</span>
+            </v-tooltip>
+            <v-tooltip bottom>
+              <template #activator="{ on, attrs }">
+                <v-btn text v-bind="attrs" rounded v-on="on">
+                  <v-icon>
+                    mdi-heart-outline
+                  </v-icon>
+                  {{ data.like }}
+                </v-btn>
+              </template>
+              <span>喜欢</span>
+            </v-tooltip>
+            <v-tooltip v-if="data.source" bottom>
+              <template #activator="{ on, attrs }">
+                <v-btn
+                  icon
+                  v-bind="attrs"
+                  rounded
+                  style="margin-left: 10px;"
+                  :to="'/projects/' + projectId + '/editor'"
+                  v-on="on"
+                >
+                  <v-icon>
+                    mdi-file-code-outline
+                  </v-icon>
+                </v-btn>
+              </template>
+              <span>源码</span>
+            </v-tooltip>
+            <v-spacer />
+            <v-menu offset-y>
+              <template #activator="{ on, attrs }">
+                <v-btn icon v-bind="attrs" v-on="on">
+                  <v-icon>
+                    mdi-dots-horizontal
+                  </v-icon>
+                </v-btn>
+              </template>
+              <v-card class="cardblur">
+                <v-list dense min-width="150">
+                  <v-list-item link>
+                    <v-list-item-icon style="margin-right: 16px;">
+                      <v-icon>mdi-share-outline</v-icon>
+                    </v-list-item-icon>
+                    <v-list-item-title>分享</v-list-item-title>
+                  </v-list-item>
+                  <v-list-item link>
+                    <v-list-item-icon style="margin-right: 16px;">
+                      <v-icon>mdi-alert-outline</v-icon>
+                    </v-list-item-icon>
+                    <v-list-item-title>举报</v-list-item-title>
+                  </v-list-item>
+                </v-list>
+              </v-card>
+            </v-menu>
+          </v-row>
         </v-card-text>
       </v-card>
       <br>
-      <ProjectDesc />
+      <v-card>
+        <v-card-title class="text-h5">
+          <span>介绍</span>
+          <v-spacer />
+          <v-btn v-if="$auth.loggedIn && data.id === $auth.user.id" icon @click="readmeOpenEdit">
+            <v-icon>mdi-pencil-outline</v-icon>
+          </v-btn>
+        </v-card-title>
+        <v-divider />
+        <MarkdownRender
+          v-if="!readmeIsEditing && data.readme"
+          :content="data.readme"
+          style="padding: 0 16px 8px 16px;"
+        />
+        <v-empty v-if="!readmeIsEditing && !data.readme" />
+        <MarkdownEditor
+          v-if="readmeIsEditing"
+          :content="data.readme"
+          textarea-label="README"
+          textarea-placeholder="介绍此作品"
+          action-icon=""
+          action-text="保存"
+          @submit="readmeSubmit"
+          @change="readmeChange"
+        />
+      </v-card>
       <div>
         <v-divider style="margin: 16px 0;" />
 
@@ -63,19 +170,24 @@
       </div>
     </v-col>
     <v-col cols="12" xl="5" md="4">
-      <ProjectUserInfo />
+      <ProjectUserInfo :user-data="data.author" />
       <br>
       //推荐作品流
     </v-col>
   </v-row>
 </template>
 <script>
-const marked = require('marked')
 export default {
   props: {
   },
+  async asyncData ({ params, $axios }) {
+    const data = await $axios.$get(`/projects/${params.id}/info`)
+    return { data: data.data }
+  },
   data: () => ({
     loadPlayer: false,
+    readmeIsEditing: false,
+    readmeEdit: '',
     bio: '啊，好舒服',
     avatar: '/GitScratch-icon-background-blue.svg',
     projectTitle: 'Default Project',
@@ -91,8 +203,50 @@ export default {
     }
   },
   methods: {
-    renderMd () {
-      return marked.parse(this.README)
+    async readmeOpenEdit () {
+      if (this.readmeIsEditing) {
+        if (this.readmeEdit === this.data.readme) {
+          this.readmeIsEditing = false
+        } else {
+          const confirm = await this.$dialog.confirm({
+            text: '编辑内容已更改，是否保存？',
+            title: '提示',
+            actions: [{
+              text: '返回编辑', key: 3
+            }, {
+              text: '不保存', key: 2
+            }, {
+              text: '保存', color: 'blue', key: 1
+            }]
+          })
+          if (confirm === 1) {
+            this.readmeSubmit(this.readmeEdit)
+          } else if (confirm === 2) {
+            this.readmeIsEditing = false
+          } else {
+            this.readmeIsEditing = true
+          }
+        }
+      } else {
+        this.readmeEdit = this.data.readme
+        this.readmeIsEditing = true
+      }
+    },
+    async readmeSubmit (n) {
+      // console.log(n)
+      const projectInfo = await this.$axios.$post('projects/' + this.$route.params.id + '/info', {
+        readme: n
+      })
+      if (projectInfo !== false) {
+        this.$dialog.message.info('保存成功', {
+          position: 'bottom'
+        })
+        this.data.readme = n
+        this.readmeIsEditing = false
+      }
+    },
+    readmeChange (n) {
+      this.readmeEdit = n
     }
   }
 }
