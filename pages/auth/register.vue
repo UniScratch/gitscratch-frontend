a23dc983505dcbe07292ae3182ffd1efdddbf5d7
TimFang <mc13735292967@163.com>
2022-08-18 15:01:12 +0800

    样式优化
    Signed-off-by: TimFang <mc13735292967@163.com>

--- pages/auth/register.vue
+++ pages/auth/register.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="d-flex justify-center">
-    <v-card style="width: 400px; padding: 16px;" elevation="0" color="transparent">
+  <div class="d-flex justify-center pt-16">
+    <v-card style="width: 400px;" elevation="0">
       <v-lazy style="text-align: center;" transition="fade-transition" min-height="71">
         <img src="/GitScratch-icon-background-blue.svg" width="64">
       </v-lazy>
