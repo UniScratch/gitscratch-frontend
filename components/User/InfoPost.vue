ca43b9da527fd4bda37e971ecfa7e1b8e15992b3
TimFang <mc13735292967@163.com>
2022-08-22 20:15:41 +0800

    添加 论坛帖子显示
    Signed-off-by: TimFang <mc13735292967@163.com>

---
+++ components/User/InfoPost.vue
@@ -0,0 +1,80 @@
+<template>
+  <v-list-item two-line>
+    <v-list-item-avatar
+      size="60"
+    >
+      <Avatar :data="userData" size="60" />
+      <!-- <v-avatar style="width: auto; height: auto; max-width: 200px;">
+          <v-img :src="$utils.getAssetUrl(userData.avatar)" size="60" />
+        </v-avatar> -->
+    </v-list-item-avatar>
+    <v-list-item-content>
+      <v-list-item-title class="text-h5 d-flex">
+        <nuxt-link :to="'/users/' + userData.id" class="project-title">
+          {{ userData.name }}
+        </nuxt-link>
+        <v-tooltip v-if="userData.verified !== 0" bottom>
+          <template #activator="{ on, attrs }">
+            <v-icon color="rgba(33,150,243)" v-bind="attrs" v-on="on">
+              mdi-check-decagram-outline
+            </v-icon>
+          </template>
+          <span>社区官方认证</span>
+        </v-tooltip>
+        <v-tooltip v-if="userData.muted !== 0" bottom>
+          <template #activator="{ on, attrs }">
+            <v-icon color="rgb(255, 87, 34)" v-bind="attrs" v-on="on">
+              mdi-comment-remove-outline
+            </v-icon>
+          </template>
+          <span>账户被禁言，{{ userData.muted }} 天后解禁</span>
+        </v-tooltip>
+        <v-tooltip v-if="userData.banned !== 0" bottom>
+          <template #activator="{ on, attrs }">
+            <v-icon color="rgb(238, 54, 37)" v-bind="attrs" v-on="on">
+              mdi-gavel
+            </v-icon>
+          </template>
+          <span>帐户被封禁，{{ userData.banned }} 天后解禁</span>
+        </v-tooltip>
+        <v-spacer />
+        <v-btn color="primary" depressed rounded>
+          <v-icon>mdi-plus</v-icon>
+          关注
+        </v-btn>
+      </v-list-item-title>
+      <!-- <span class="level-span">Lv. {{ userData.level }}</span> -->
+      <v-list-item-subtitle>{{ userData.bio }}</v-list-item-subtitle>
+    </v-list-item-content>
+  </v-list-item>
+</template>
+
+<script>
+export default {
+  props: {
+    userData: {
+      type: Object,
+      default: () => ({})
+    }
+  },
+  data: () => ({
+  })
+}
+</script>
+<style>
+.level-span{
+  position: absolute;
+  text-align: center;
+  max-width: 52px;
+  padding-right: 8px;
+  padding-left: 8px;
+  height: 20px;
+  background-color: rgba(34, 149, 242);
+  color: white;
+  border-radius: 20px;
+  font-size: 12px;
+  padding-top: 3px;
+  top: 20%;
+  right: 5%;
+}
+</style>
---
+++ pages/forum/_id.vue
@@ -0,0 +1,154 @@
+<template>
+  <div>
+    <v-card>
+      <v-card-title class="text-h5">
+        {{ !contentIsEditing ? data.title : '' }}
+        <v-text-field
+          v-if="contentIsEditing"
+          v-model="titleEdit"
+          label="标题"
+          outlined
+          hide-details="auto"
+        />
+        &nbsp;
+        <v-chip
+          v-if="data.status === 0"
+          color="primary"
+          outlined
+          ripple
+        >
+          未发布
+        </v-chip>
+        <v-spacer />
+        <v-btn text @click="contentSubmit({status:1-data.status})">
+          <v-icon>mdi-upload-outline</v-icon>
+          {{ data.status === 0 ? '' : "取消" }}发布
+        </v-btn>
+        <v-btn text @click="contentOpenEdit()">
+          <v-icon>mdi-pencil-outline</v-icon>
+          编辑
+        </v-btn>
+      </v-card-title>
+      <v-card-text>
+        发布于 {{ $utils.getTimeString(data.created_at) }}，最后更新 {{ $utils.getTimeString(data.updated_at) }}
+      </v-card-text>
+      <UserInfoPost :user-data="data.user" />
+      <v-divider />
+      <MarkdownRender
+        v-if="!contentIsEditing && data.content"
+        :content="data.content"
+        style="padding: 0 16px 8px 16px;"
+      />
+      <v-empty v-if="!contentIsEditing && !data.content" />
+      <MarkdownEditor
+        v-if="contentIsEditing"
+        :content="data.content"
+        textarea-label="content"
+        textarea-placeholder="畅所欲言"
+        action-icon=""
+        action-text="保存"
+        @submit="(n) => contentSubmit({content:n})"
+        @change="contentChange"
+      />
+    </v-card>
+    <br>
+    <CommentGroup />
+  </div>
+</template>
+<script>
+export default {
+  data: () => ({
+    data: { },
+    contentIsEditing: false,
+    contentEdit: '',
+    titleEdit: ''
+  }),
+  async fetch () {
+    const data = await this.$axios.$get(`/posts/${this.$route.params.id}/info`)
+    this.data = data.data
+  },
+  head () {
+    return {
+      title: this.data.title
+    }
+  },
+  methods: {
+    async contentOpenEdit () {
+      if (this.contentIsEditing) {
+        if (this.contentEdit === this.data.content && this.titleEdit === this.data.title) {
+          this.contentIsEditing = false
+        } else {
+          const confirm = await this.$dialog.confirm({
+            text: '编辑内容已更改，是否保存？',
+            title: '提示',
+            actions: [{
+              text: '返回编辑', key: 3
+            }, {
+              text: '不保存', key: 2
+            }, {
+              text: '保存', color: 'blue', key: 1
+            }]
+          })
+          if (confirm === 1) {
+            this.contentSubmit({ content: this.contentEdit })
+          } else if (confirm === 2) {
+            this.contentIsEditing = false
+          } else {
+            this.contentIsEditing = true
+          }
+        }
+      } else {
+        this.contentEdit = this.data.content
+        this.titleEdit = this.data.title
+        this.contentIsEditing = true
+      }
+    },
+    async contentSubmit (n) {
+    //   console.log(n)
+      const projectInfo = await this.$axios.$post('posts/' + this.$route.params.id + '/info', {
+        content: Object.prototype.hasOwnProperty.call(n, 'content') ? n.content : undefined,
+        title: this.titleEdit,
+        status: Object.prototype.hasOwnProperty.call(n, 'status') ? n.status : undefined
+      })
+      if (projectInfo !== false) {
+        this.$dialog.message.info('保存成功', {
+          position: 'bottom'
+        })
+        await this.$fetch()
+        this.contentIsEditing = false
+      }
+    },
+    contentChange (n) {
+      this.contentEdit = n
+    },
+    async operation (type) {
+      await this.$axios.$post('posts/' + this.$route.params.id + '/operation', {
+        type
+      })
+      this.$fetch()
+      // console.log(res)
+    },
+    async like () {
+      await this.operation('project.like')
+      this.$dialog.message.info('点赞成功', {
+        position: 'bottom'
+      })
+    },
+    star () {
+      this.$dialog.message.info('收藏成功', {
+        position: 'bottom'
+      })
+    },
+    fork () {
+      this.$dialog.message.info('分享成功', {
+        position: 'bottom'
+      })
+    },
+    view () {
+      this.$dialog.message.info('查看成功', {
+        position: 'bottom'
+      })
+    }
+  }
+}
+</script>
